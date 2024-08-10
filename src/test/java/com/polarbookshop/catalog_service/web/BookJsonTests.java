fcbd92064c2dce1744e639773a0f3c1f36787a00
codingkid <jjg1014@naver.com>
2024-08-10 17:38:56 +0900

    feat: Json 직렬화 슬라이스 테스트 추가

---
+++ src/test/java/com/polarbookshop/catalog_service/web/BookJsonTests.java
@@ -0,0 +1,56 @@
+package com.polarbookshop.catalog_service.web;
+
+import com.polarbookshop.catalog_service.domain.Book;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Json 직렬화에 중점을 둔 테스트
+ */
+@JsonTest
+public class BookJsonTests {
+
+    @Autowired
+    private JacksonTester<Book> json;
+
+    @Test
+    void testSerialize() throws Exception {
+        var book = new Book("1234567890", "Title", "Author", 9.90);
+
+        var jsonContent = json.write(book);
+
+        // 필드값 얻는법 @.isbn 이렇게 하면 얻을수 있음
+
+        assertThat(jsonContent)
+                .extractingJsonPathStringValue("@.isbn")
+                .isEqualTo(book.isbn());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.title")
+                .isEqualTo(book.title());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.author")
+                .isEqualTo(book.author());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
+                .isEqualTo(book.price());
+    }
+
+    @Test
+    void testDeserialize() throws Exception {
+        var content = """
+                {
+                    "isbn": "1234567890",
+                    "title": "Title",
+                    "author": "Author",
+                    "price": 9.90
+                }
+                """;
+
+        // Json에서 자바 객체로 역직렬화
+        assertThat(json.parse(content))
+                .usingRecursiveComparison()
+                .isEqualTo(new Book("1234567890", "Title", "Author", 9.90));
+    }
+}
