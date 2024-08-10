4c41db1440727abffcbc50fa6ddb36df126a0724
codingkid <jjg1014@naver.com>
2024-08-10 17:29:51 +0900

    feat: 웹플러스를 사용한 카탈로그 서비스 통합 테스트 추가

--- src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
@@ -1,13 +1,39 @@
 package com.polarbookshop.catalog_service;
 
+import com.polarbookshop.catalog_service.domain.Book;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+		// 완전한 스프링 웹 애플리케이션 콘텍스트와 임의의 포트를 듣는 서블릿 컨테이너를 로드
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
 
+	// 테스트를 위해 REST 엔드포인트를 호출할 유틸리티
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void whenPostRequestThenBookCreated() {
+		var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook) // 요청 본문에 Book 객체 추가
+				.exchange() //요청 전송
+				.expectStatus().isCreated() // HTTP 응답코드 201 상태 확인
+				.expectBody(Book.class)
+				.value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+				});
 	}
 
 }
