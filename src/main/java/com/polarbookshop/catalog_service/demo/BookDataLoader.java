ddfb424253ec835f860585cd0f9fa7a64bb427b9
codingkid <jjg1014@naver.com>
2024-09-04 22:19:12 +0900

    refactor: 초기 데이터 추가 리팩토링

--- src/main/java/com/polarbookshop/catalog_service/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalog_service/demo/BookDataLoader.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata") // 이 클래스는 testdata 프로파일 활성화일때만 로드됨
 @RequiredArgsConstructor
@@ -22,9 +24,9 @@ public class BookDataLoader {
      */
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
-        var book1 = new Book("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
-        var book2 = new Book("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.deleteAll();
+        var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silvertongue", 9.90, "Polarsophia");
+        var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90, "Polarsophia");
+        bookRepository.saveAll(List.of(book1, book2));
     }
 }
