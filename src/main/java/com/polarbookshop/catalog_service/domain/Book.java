3e82c192ac96ae629f9cd6b5833788c211ce5f12
codingkid <jjg1014@naver.com>
2024-09-04 22:18:32 +0900

    refactor: JPA Book 도메인, 리포지토리, 서비스 리팩토링

--- src/main/java/com/polarbookshop/catalog_service/domain/Book.java
+++ src/main/java/com/polarbookshop/catalog_service/domain/Book.java
@@ -1,30 +1,91 @@
 package com.polarbookshop.catalog_service.domain;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-/**
- * 도메인 모델은 불가변 객체인 레코드로 구현
- * @param isbn 책 고유 식별
- * @param title
- * @param author
- * @param price
- */
-public record Book(
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter // setter 연습용
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+        @Id @GeneratedValue(strategy = IDENTITY)
+        private Long id;
+
         @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
-        String isbn,
+        private String isbn;
 
         @NotBlank(message = "The book title must be defined.")
-        String title,
+        private String title;
 
         @NotBlank(message = "The book author must be defined.")
-        String author,
+        private String author;
 
         @NotNull(message = "The book price must be defined.")
         @Positive(message = "The book price must be greater than zero.") // 필드는 null이 아니고 0보다 큰값이여야한다.
-        Double price
-) {
+        private Double price;
+
+        private String publisher;
+
+        @CreatedDate
+        private LocalDateTime createdDate;
+
+        @LastModifiedDate
+        private LocalDateTime lastModifiedDate;
+
+        @Version
+        private int version;
+
+        public static Book of(String isbn, String title, String author, Double price, String publisher) {
+                return new Book(
+                        null,
+                        isbn,
+                        title,
+                        author,
+                        price,
+                        publisher,
+                        null,
+                        null,
+                        0
+                );
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Book book = (Book) o;
+                return version == book.version &&
+                        id.equals(book.id) &&
+                        isbn.equals(book.isbn) &&
+                        title.equals(book.title) &&
+                        author.equals(book.author) &&
+                        price.equals(book.price) &&
+                        Objects.equals(publisher, book.publisher) &&
+                        createdDate.equals(book.createdDate) &&
+                        lastModifiedDate.equals(book.lastModifiedDate);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(id, isbn, title, author, price, publisher, createdDate, lastModifiedDate, version);
+        }
 }
+
+
--- src/main/java/com/polarbookshop/catalog_service/domain/BookService.java
+++ src/main/java/com/polarbookshop/catalog_service/domain/BookService.java
@@ -2,25 +2,29 @@ package com.polarbookshop.catalog_service.domain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class BookService {
 
     private final BookRepository bookRepository;
 
+    @Transactional(readOnly = true)
     public Iterable<Book> viewBookList() {
         return bookRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Book viewBookDetails(String isbn) {
         return bookRepository.findByIsbn(isbn)
                 .orElseThrow(() -> new BookNotFoundException(isbn));
     }
 
     public Book addBookToCatalog(Book book) {
-        if (bookRepository.existsByIsbn(book.isbn())) {
-            throw new BookAlreadyExistsException(book.isbn());
+        if (bookRepository.existsByIsbn(book.getIsbn())) {
+            throw new BookAlreadyExistsException(book.getIsbn());
         }
         return bookRepository.save(book);
     }
@@ -32,13 +36,11 @@ public class BookService {
     public Book editBookDetails(String isbn, Book book) {
         return bookRepository.findByIsbn(isbn)
                 .map(existingBook -> {
-                    var bookToUpdate = new Book(
-                            existingBook.isbn(),
-                            book.title(),
-                            book.author(),
-                            book.price()
-                    );
-                    return bookRepository.save(bookToUpdate);
+                    existingBook.setTitle(book.getTitle());
+                    existingBook.setAuthor(book.getAuthor());
+                    existingBook.setPrice(book.getPrice());
+                    existingBook.setPublisher(book.getPublisher());
+                    return bookRepository.save(existingBook);
                 })
                 .orElseGet(() -> addBookToCatalog(book));
     }
