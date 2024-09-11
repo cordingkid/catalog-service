cc897a0760151a1d812fa111d85c2f06e01d11ae
codingkid <jjg1014@naver.com>
2024-09-11 22:07:12 +0900

    refactor: 테스트코드 리팩토링

--- src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,11 +14,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 		// 완전한 스프링 웹 애플리케이션 콘텍스트와 임의의 포트를 듣는 서블릿 컨테이너를 로드
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
 	// 테스트를 위해 REST 엔드포인트를 호출할 유틸리티
 	@Autowired
 	private WebTestClient webTestClient;
+
+	@Test
+	void whenGetRequestWithIdThenBookReturned() {
+		var bookIsbn = "1231231230";
+		var bookToCreate = Book.of(bookIsbn, "Title", "Author", 9.90, "Polarsophia");
+		Book expectedBook = webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(bookToCreate)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+				.returnResult().getResponseBody();
+
+		webTestClient
+				.get()
+				.uri("/books/" + bookIsbn)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.getIsbn()).isEqualTo(expectedBook.getIsbn());
+				});
+	}
 
 	@Test
 	void whenPostRequestThenBookCreated() {
@@ -34,6 +60,59 @@ class CatalogServiceApplicationTests {
 					assertThat(actualBook).isNotNull();
 					assertThat(actualBook.getIsbn()).isEqualTo(expectedBook.getIsbn());
 				});
+	}
+
+	@Test
+	void whenPutRequestThenBookUpdated() {
+		var bookIsbn = "1231231232";
+		var bookToCreate = Book.of(bookIsbn, "Title", "Author", 9.90, "Polarsophia");
+		Book createdBook = webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(bookToCreate)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+				.returnResult().getResponseBody();
+		createdBook.setPrice(7.95);
+
+		webTestClient
+				.put()
+				.uri("/books/" + bookIsbn)
+				.bodyValue(createdBook)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull();
+					assertThat(actualBook.getPrice()).isEqualTo(createdBook.getPrice());
+				});
+	}
+
+	@Test
+	void whenDeleteRequestThenBookDeleted() {
+		var bookIsbn = "1231231233";
+		var bookToCreate = Book.of(bookIsbn, "Title", "Author", 9.90, "Polarsophia");
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(bookToCreate)
+				.exchange()
+				.expectStatus().isCreated();
+
+		webTestClient
+				.delete()
+				.uri("/books/" + bookIsbn)
+				.exchange()
+				.expectStatus().isNoContent();
+
+		webTestClient
+				.get()
+				.uri("/books/" + bookIsbn)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectBody(String.class).value(errorMessage ->
+						assertThat(errorMessage).isEqualTo("The book with ISBN " + bookIsbn + " was not found.")
+				);
 	}
 
 }
---
+++ src/test/java/com/polarbookshop/catalog_service/domain/BookRepositoryJdbcTests.java
@@ -0,0 +1,96 @@
+package com.polarbookshop.catalog_service.domain;
+
+import com.polarbookshop.catalog_service.config.DataConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(
+        // 테스트컨테이너를 이용해야 하기 때문에 내장 테스트 데이터베이스 사용을 비활성화
+        replace = AutoConfigureTestDatabase.Replace.NONE
+)
+@ActiveProfiles("integration") // application-integration.yml 에서 설정을 로드하기 위해 활성화
+public class BookRepositoryJdbcTests {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    void findAllBooks(){
+        var book1 = Book.of("1234561235", "Title", "Author", 12.90, "Polarsophia");
+        var book2 = Book.of("1234561236", "Another Title", "Author", 12.90, "Polarsophia");
+        entityManager.persist(book1);
+        entityManager.persist(book2);
+
+        List<Book> actualBooks = bookRepository.findAll();
+
+        assertThat(StreamSupport.stream(actualBooks.spliterator(), true)
+                .filter(book -> book.getIsbn().equals(book1.getIsbn()) ||
+                        book.getIsbn().equals(book2.getIsbn()))
+                .collect(Collectors.toList())).hasSize(2);
+    }
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        var bookIsbn = "1234561237";
+        var book = Book.of(bookIsbn, "Title", "Author", 12.90, "Polarsophia");
+        entityManager.persist(book);
+
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().getIsbn()).isEqualTo(book.getIsbn());
+    }
+
+    @Test
+    void findBookByIsbnWhenNotExisting() {
+        Optional<Book> actualBook = bookRepository.findByIsbn("1234561238");
+        assertThat(actualBook).isEmpty();
+    }
+
+    @Test
+    void existsByIsbnWhenExisting() {
+        var bookIsbn = "1234561239";
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 12.90, "Polarsophia");
+        entityManager.persist(bookToCreate);
+
+        boolean existing = bookRepository.existsByIsbn(bookIsbn);
+
+        assertThat(existing).isTrue();
+    }
+
+    @Test
+    void existsByIsbnWhenNotExisting() {
+        boolean existing = bookRepository.existsByIsbn("1234561240");
+        assertThat(existing).isFalse();
+    }
+
+    @Test
+    void deleteByIsbn() {
+        var bookIsbn = "1234561241";
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 12.90, "Polarsophia");
+        var persistedBook = entityManager.persist(bookToCreate);
+
+        bookRepository.deleteByIsbn(bookIsbn);
+
+        assertThat(entityManager.find(Book.class, persistedBook.getId())).isNull();
+    }
+}
