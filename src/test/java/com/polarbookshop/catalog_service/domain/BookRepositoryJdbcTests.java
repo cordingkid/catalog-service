810e37f561af7cd917469752453f23a5435cc897
codingkid <jjg1014@naver.com>
2024-10-10 22:04:53 +0900

    배포파이프라인 테스트

--- src/test/java/com/polarbookshop/catalog_service/domain/BookRepositoryJdbcTests.java
+++ src/test/java/com/polarbookshop/catalog_service/domain/BookRepositoryJdbcTests.java
@@ -1,7 +1,6 @@
 package com.polarbookshop.catalog_service.domain;
 
 import com.polarbookshop.catalog_service.config.DataConfig;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -9,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 import java.util.Optional;
