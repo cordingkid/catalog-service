2f371726cf67f1715ae48088c959652525bbbe87
codingkid <jjg1014@naver.com>
2025-01-16 21:15:56 +0900

    feat: Jwt 역할과 부여된 권한 매핑 및 employee 역할 사용자에게만 쓰기 엑스

--- src/main/java/com/polarbookshop/catalog_service/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/catalog_service/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
@@ -17,11 +19,22 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().hasRole("employee"))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();      // 클레임을 GrantedAuthority 객체로 매핑하기 위한 변환기
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");                     // 각 사용자 역할에 ROLE_ 접두어를 붙힘
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");                // roles클레임에서 역할 추출
+
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();              // JWT를 변환할 방법 정의
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 }
