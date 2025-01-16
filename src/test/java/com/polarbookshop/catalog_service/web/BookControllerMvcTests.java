c25c79be449d2c540be8211c4578b9363f54a87d
codingkid <jjg1014@naver.com>
2025-01-16 21:58:07 +0900

    test: BookController 권한 테스트 작성

--- src/test/java/com/polarbookshop/catalog_service/web/BookControllerMvcTests.java
+++ src/test/java/com/polarbookshop/catalog_service/web/BookControllerMvcTests.java
@@ -1,36 +1,171 @@
 package com.polarbookshop.catalog_service.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.polarbookshop.catalog_service.config.SecurityConfig;
+import com.polarbookshop.catalog_service.domain.Book;
 import com.polarbookshop.catalog_service.domain.BookNotFoundException;
 import com.polarbookshop.catalog_service.domain.BookService;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
+@Import(SecurityConfig.class)
 public class BookControllerMvcTests {
+
+    private static final String ROLE_EMPLOYEE = "ROLE_employee";
+    private static final String ROLE_CUSTOMER = "ROLE_customer";
 
     // 모의 환경에서 웹 계층을 테스트하기 위한 유틸리티 클래스
     // 톰캣 같은 서버를 로드하지 않고도 웹 엔드포인트 테스트가능
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     // 스프링 애플리케이션 콘텍스트에 모의 객체 추가
     @MockBean
     private BookService bookService;
 
     @Test
-    void whenGetBookBotExistingThenShouldReturn404() throws Exception {
-        String isbn = "73737313940";
+    void whenGetBookExistingAndAuthenticatedThenShouldReturn200() throws Exception {
+        var isbn = "7373731394";
+        var expectedBook = Book.of(isbn, "Title", "Author", 9.90, "Polarsophia");
+        given(bookService.viewBookDetails(isbn)).willReturn(expectedBook);
+        mockMvc.perform(get("/books/" + isbn)
+                        .with(jwt()))
+                .andExpect(status().isOk());
+    }
 
-        BDDMockito.given(bookService.viewBookDetails(isbn))
-                .willThrow(BookNotFoundException.class);
+    @Test
+    void whenGetBookExistingAndNotAuthenticatedThenShouldReturn200() throws Exception {
+        var isbn = "7373731394";
+        var expectedBook = Book.of(isbn, "Title", "Author", 9.90, "Polarsophia");
+        given(bookService.viewBookDetails(isbn)).willReturn(expectedBook);
+        mockMvc.perform(get("/books/" + isbn))
+                .andExpect(status().isOk());
+    }
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/books/" + isbn))
-                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    @Test
+    void whenGetBookNotExistingAndAuthenticatedThenShouldReturn404() throws Exception {
+        var isbn = "7373731394";
+        given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
+        mockMvc.perform(get("/books/" + isbn)
+                        .with(jwt()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void whenGetBookNotExistingAndNotAuthenticatedThenShouldReturn404() throws Exception {
+        var isbn = "7373731394";
+        given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
+        mockMvc.perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void whenDeleteBookWithEmployeeRoleThenShouldReturn204() throws Exception {
+        var isbn = "7373731394";
+        mockMvc.perform(delete("/books/" + isbn)
+                        .with(jwt().authorities(new SimpleGrantedAuthority(ROLE_EMPLOYEE))))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void whenDeleteBookWithCustomerRoleThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        mockMvc.perform(delete("/books/" + isbn)
+                        .with(jwt().authorities(new SimpleGrantedAuthority(ROLE_CUSTOMER))))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void whenDeleteBookNotAuthenticatedThenShouldReturn401() throws Exception {
+        var isbn = "7373731394";
+        mockMvc.perform(delete("/books/" + isbn))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void whenPostBookWithEmployeeRoleThenShouldReturn201() throws Exception {
+        var isbn = "7373731394";
+        var bookToCreate = Book.of(isbn, "Title", "Author", 9.90, "Polarsophia");
+        given(bookService.addBookToCatalog(bookToCreate)).willReturn(bookToCreate);
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bookToCreate))
+                        .with(jwt().authorities(new SimpleGrantedAuthority(ROLE_EMPLOYEE))))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void whenPostBookWithCustomerRoleThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        var bookToCreate = Book.of(isbn, "Title", "Author", 9.90, "Polarsophia");
+        given(bookService.addBookToCatalog(bookToCreate)).willReturn(bookToCreate);
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bookToCreate))
+                        .with(jwt().authorities(new SimpleGrantedAuthority(ROLE_CUSTOMER))))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void whenPostBookAndNotAuthenticatedThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        var bookToCreate = Book.of(isbn, "Title", "Author", 9.90, "Polarsophia");
+        mockMvc.perform(post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bookToCreate)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void whenPutBookWithEmployeeRoleThenShouldReturn200() throws Exception {
+        var isbn = "7373731394";
+        var bookToCreate = Book.of(isbn, "Title", "Author", 9.90, "Polarsophia");
+        given(bookService.addBookToCatalog(bookToCreate)).willReturn(bookToCreate);
+        mockMvc.perform(put("/books/" + isbn)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bookToCreate))
+                        .with(jwt().authorities(new SimpleGrantedAuthority(ROLE_EMPLOYEE))))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenPutBookWithCustomerRoleThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        var bookToCreate = Book.of(isbn, "Title", "Author", 9.90, "Polarsophia");
+        given(bookService.addBookToCatalog(bookToCreate)).willReturn(bookToCreate);
+        mockMvc.perform(put("/books/" + isbn)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bookToCreate))
+                        .with(jwt().authorities(new SimpleGrantedAuthority(ROLE_CUSTOMER))))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void whenPutBookAndNotAuthenticatedThenShouldReturn401() throws Exception {
+        var isbn = "7373731394";
+        var bookToCreate = Book.of(isbn, "Title", "Author", 9.90, "Polarsophia");
+        mockMvc.perform(put("/books/" + isbn)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(bookToCreate)))
+                .andExpect(status().isUnauthorized());
     }
 }
