966fbd0f4f187548ecf8156688e712518b08f1eb
codingkid <jjg1014@naver.com>
2025-02-06 21:42:11 +0900

    chore: 스프링부트 엑추에이터 상태프로브 설정 추가

--- src/main/java/com/polarbookshop/catalog_service/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/catalog_service/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
                         .anyRequest().hasRole("employee"))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
